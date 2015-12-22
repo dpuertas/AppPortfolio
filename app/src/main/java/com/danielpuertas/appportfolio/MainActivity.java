6bb420a35ca1ef36b3814687c343d0f2e516d6b3
dpuertas <dpuertas@gmail.com>
2015-12-22 01:25:09 -0800

    Add click listeners and toast action

--- app/src/main/java/com/danielpuertas/appportfolio/MainActivity.java
+++ app/src/main/java/com/danielpuertas/appportfolio/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +20,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-/*
-         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+//Set Click Listeners
+
+        //Spotify Button
+        final Button spotButton = (Button) findViewById(R.id.spotifyButton);
+        spotButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(spotButton);
             }
         });
-        */
+
+        //Scores App
+        final Button scoresButton = (Button) findViewById(R.id.scoresAppButton);
+        scoresButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(scoresButton);
+            }
+        });
+
+        //Library App
+        final Button libButton = (Button) findViewById(R.id.libraryAppButton);
+        libButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(libButton);
+            }
+        });
+
+        //Build it Bigger
+        final Button buildButton = (Button) findViewById(R.id.buildButton);
+        buildButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(buildButton);
+            }
+        });
+
+        //XYZ Reader
+        final Button xyzButton = (Button) findViewById(R.id.xyzButton);
+        xyzButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(xyzButton);
+            }
+        });
+
+        //Capstone App
+        final Button capButton = (Button) findViewById(R.id.capstoneButton);
+        capButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showButtonToast(capButton);
+            }
+        });
+
+    }
+
+    //Show a toast - placeholder
+    public boolean showButtonToast(Button myButton) {
+        CharSequence text = "This button will launch " +  myButton.getText();
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        return true;
     }
 
     @Override
